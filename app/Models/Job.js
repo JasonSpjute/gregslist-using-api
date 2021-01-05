dcb9c78c19303e3502f105c13a273895db21e127
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-05 16:23:48 -0700

    built job html and job js

---
+++ app/Models/Job.js
@@ -0,0 +1,32 @@
+export default class Job{
+    constructor(company, jobTitle, hours, rate, description, id){
+        this.company = company
+        this.jobTitle = jobTitle
+        this.hours = hours
+        this.rate = rate
+        this.description = description
+        this.id = id
+    }
+
+    get Template(){
+        return /*html*/`
+        <div class="col-md-4 col-6 mt-3">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">${this.jobTitle}</h4>
+                <h6 class="card-subtitle">${this.company}</h6>
+                <p class="card-text">Rate = ${this.rate}</p>
+                <p class="card-text">Hours: ${this.hours}</p>
+                <p class="card-text">Bathrooms: ${this.description}</p>
+                <div class="text-right">
+                    <button type="button" class="btn btn-success" onclick="app.housesController.bid('${this.id}', '${this.price += 500}')">Bid</button>
+                    <button type="button" class="btn btn-danger" onclick="app.housesController.deleteHouse('${this.id}')">Delete</button>
+                </div>
+            </div>
+        </div>
+        </div>
+            `
+    
+    }
+    }
+}
